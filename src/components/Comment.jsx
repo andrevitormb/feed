c06f51e9effcbe8fb350a05bdfea8880c29c0e0b
andre macedo <andrevitoreur@gmail.com>
2023-12-19 23:23:03 +0100

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+export function Comment (){
+    return (
+        <div className={styles.comment}>
+            
+            <Avatar hasBorder={false} src='https://github.com/diego3g.png' />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                    <strong>Andre Macedo</strong>
+                    <time title="20 of Dezember by 08:13" dateTime="2022-05-11 08:13:00">About 1 hour ago</time>
+                    </div>
+
+                    <button title="Delete Comment">
+                    <Trash size={24} />
+                    </button>
+                </header>
+
+                <p>Muito bom andre, Parabens </p>
+                </div>
+
+                <footer>
+                <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,50 @@
+import {format, formatDistanceToNow} from 'date-fns'
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+
+export function Post({author,publishedAt,content}) {
+    const publishedDateFormatted = format(publishedAt, "d 'of' LLLL 'at' HH:mm ")
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+        addSuffix: true ,
+    })
+    return (
+    <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <Avatar hasBorder src={author.authorUrl}/>
+                <div className={styles.authorInfo}>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
+                </div>
+            </div>
+            <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+        </header>
+        <div className={styles.content}>
+            {content.map(line => {
+                if(line.type ==='paragraph'){
+                    return <p>{line.content}</p>
+                }else if (line.type ==='link'){
+                    return <p><a href='#'>{line.content}</a></p>
+                }
+                
+            })}
+        </div>
+        <form className={styles.commentForm} action="">
+            <strong>Type your Feedback</strong>
+            <textarea 
+                placeholder='Type your comment' 
+            />
+            <footer>
+                <button type='submit'>Comment</button>
+            </footer>
+        </form>
+        <div className={styles.commentList}>
+            <Comment/>
+            <Comment/>
+            <Comment/>
+        </div>
+    </article>
+  )
+}
